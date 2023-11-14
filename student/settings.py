a1b27f5d6dd942c34341beea317d25d3d80f8226
Prakash Pramanik <prakashpramanik280@gmail.com>
2023-11-14 12:26:27 +0530

    some more  text

--- student/settings.py
+++ student/settings.py
@@ -127,9 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL='/static/'
-STATICFILES_DIRS=[BASE_DIR/'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
